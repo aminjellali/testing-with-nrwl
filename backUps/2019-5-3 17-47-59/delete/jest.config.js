3f54b524fa7a2c55509625f56e93527c7f2e79ef
Amin Jellali <amin.jellali@ads.lan>
2019-05-07 12:16:25 +0100

    start using git ignore

--- backUps/2019-5-3 17-47-59/delete/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'delete',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/delete/',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
